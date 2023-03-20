74ea7c8af0a0fa0f93fbc34a1d7fdf366baa41da
perry1224 <perry.w1224@gmail.com>
2023-03-20 15:31:22 -0400

    Update ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -15,9 +15,13 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle:event.target.value,
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle:event.target.value,
+    // })
+    //Ensures the latest state updates
+    setUserInput((prevState) => {
+      return {...prevState, enteredTitle: event.target.value}
     })
   };
 
